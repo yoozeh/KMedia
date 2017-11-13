07fb318b0d105bc943e984532295370f06ae2b3a
yoozeh <yoozeh@naver.com>
2017-11-13 15:55:26 +0900

    Added basic server code for heroku

---
+++ src/server/index.ts
@@ -0,0 +1,17 @@
+import * as express from 'express';
+import * as http from 'http';
+import * as path from 'path';
+
+const port = process.env.PORT || 8000;
+const app = express();
+const httpServer = http.createServer(app);
+
+app.use('/', express.static(path.resolve(__dirname, '../..', 'dist-app')));
+app.use('/!static', express.static(path.resolve(__dirname, '../..', 'static')));
+app.all('*', (request, response) => {
+  response.sendFile(path.resolve(__dirname, '../..', 'dist-app/index.html'));
+});
+
+httpServer.listen(port, () => {
+  console.log('HTTP Server is listening on %s', httpServer.address().port);
+});
